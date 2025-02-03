e40c06b0889ab548a7362fb8b059e2a689dee6de
Sunhuawei <stonesun@alipay.com>
2025-02-03 23:26:38 +0800

    a

--- src/utils/browser-polyfill.ts
+++ src/utils/browser-polyfill.ts
@@ -2,13 +2,10 @@
 import browser from 'webextension-polyfill';
 
 export const browserAPI = {
-    storage: browser.storage,
+    action: browser.action,
     runtime: browser.runtime,
     tabs: browser.tabs,
-    downloads: browser.downloads,
     webRequest: browser.webRequest,
-    declarativeNetRequest: browser.declarativeNetRequest,
-    action: browser.action,
     windows: browser.windows
 };
 
@@ -18,4 +15,3 @@ export type BrowserRuntime = typeof browser.runtime;
 export type BrowserTabs = typeof browser.tabs;
 export type BrowserAction = typeof browser.action;
 export type BrowserWebRequest = typeof browser.webRequest;
-export type BrowserDeclarativeNetRequest = typeof browser.declarativeNetRequest; 
